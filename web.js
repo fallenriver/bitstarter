ac658fe62c6180571be826f654fb95dbce92b15a
Ubuntu <ubuntu@ip-10-164-49-31.ec2.internal>
2013-07-12 19:25:13 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ app.get('/', function(request, response)
 {
   var output = fs.readFileSync('index.html');
   response.send(output.toString);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
